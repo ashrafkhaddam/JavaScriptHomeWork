bd094ea8547392968bbf3af1cfeadf14a5d174f5
Ashraf khaddam <ashrafkh1@gmail.com>
2017-05-07 01:35:05 -0700

    Update homework.js

--- assignment 4/homework.js
+++ assignment 4/homework.js
@@ -1,46 +1,55 @@
-var searchResults = {"Search":[{"Title":"Black Cat, White Cat","Year":"1998","imdbID":"tt0118843","Type":"movie","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMmExZTZhN2QtMzg5Mi00Y2M5LTlmMWYtNTUzMzUwMGM2OGQ3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"},{"Title":"The Cat in the Hat","Year":"2003","imdbID":"tt0312528","Type":"movie","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTI5MDU3MTYyMF5BMl5BanBnXkFtZTYwODgyODc3._V1_SX300.jpg"},{"Title":"Cat on a Hot Tin Roof","Year":"1958","imdbID":"tt0051459","Type":"movie","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMzFhNTMwNDMtZjY3Yy00NzY3LWI1ZWQtZTQxMWJmODVhZWFkXkEyXkFqcGdeQXVyNjQzNDI3NzY@._V1_SX300.jpg"},{"Title":"The Cat Returns","Year":"2002","imdbID":"tt0347618","Type":"movie","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ5ODMyNTgzOV5BMl5BanBnXkFtZTcwNDM4ODAyNw@@._V1_SX300.jpg"},{"Title":"Cat People","Year":"1982","imdbID":"tt0083722","Type":"movie","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ1MjI5MDE2M15BMl5BanBnXkFtZTgwMTY0MjEyMDE@._V1_SX300.jpg"},{"Title":"Cat People","Year":"1942","imdbID":"tt0034587","Type":"movie","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BNzI4YWY0NWQtNWI5YS00MGE4LWE4YTgtMzBmOWIwMzdiYTRiL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg"},{"Title":"A Street Cat Named Bob","Year":"2016","imdbID":"tt3606888","Type":"movie","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTY5MTI1MzE5Nl5BMl5BanBnXkFtZTgwMjQzNjEzOTE@._V1_SX300.jpg"},{"Title":"Cat Ballou","Year":"1965","imdbID":"tt0059017","Type":"movie","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ2MDA4MzA5Nl5BMl5BanBnXkFtZTcwMjQ1NjkxMQ@@._V1._CR0,0,267,428_SX89_AL_.jpg_V1_SX300.jpg"},{"Title":"A Cat in Paris","Year":"2010","imdbID":"tt1673702","Type":"movie","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTAzODU0NDExMTNeQTJeQWpwZ15BbWU3MDk2NjMxMzc@._V1_SX300.jpg"},{"Title":"Fritz the Cat","Year":"1972","imdbID":"tt0068612","Type":"movie","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BODAzMjI5YzMtNWI5Yy00NTllLWE0MWQtOWMzZTRjMmYyYTFlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"}],"totalResults":"1057","Response":"True"};
-console.log(searchResults.Search[2].Title);
 
-var click = document.getElementById("click");
+var objects = {"Search":[{"Title":"A Very Long Engagement","Year":"2004","imdbID":"tt0344510","Type":"movie","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BZWI4ZTgwMzktNjk3Yy00OTlhLTg3YTAtMTA1MWVlMWJiOTRiXkEyXkFqcGdeQXVyMTAwMzUyOTc@._V1_SX300.jpg"},{"Title":"The Long Kiss Goodnight","Year":"1996","imdbID":"tt0116908","Type":"movie","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BNTkzOWZkN2QtNDJkYy00OTdjLThlNDQtNDg4MjMyMWE5Y2U5XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"},{"Title":"A Long Way Down","Year":"2014","imdbID":"tt0458413","Type":"movie","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTc0MTg0MDI2Ml5BMl5BanBnXkFtZTgwNzM4MDYwMTE@._V1_SX300.jpg"},{"Title":"A Love Song for Bobby Long","Year":"2004","imdbID":"tt0369672","Type":"movie","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ5MjIxNzY0M15BMl5BanBnXkFtZTcwMzM0NDcyMQ@@._V1_SX300.jpg"},{"Title":"Mandela: Long Walk to Freedom","Year":"2013","imdbID":"tt2304771","Type":"movie","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTg1NTQ1NDczNV5BMl5BanBnXkFtZTcwNDQyMDU1OQ@@._V1_SX300.jpg"},{"Title":"The Long Goodbye","Year":"1973","imdbID":"tt0070334","Type":"movie","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BYzZhODNiOWYtMmNkNS00OTFhLTkzYzktYTQ4ZmNmZWMyN2ZiL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg"},{"Title":"I've Loved You So Long","Year":"2008","imdbID":"tt1068649","Type":"movie","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTY1Njg2ODI1Ml5BMl5BanBnXkFtZTcwMzkzNzQ5MQ@@._V1_SX300.jpg"},{"Title":"The Long Good Friday","Year":"1980","imdbID":"tt0081070","Type":"movie","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMjAzNDMxNTQ1OV5BMl5BanBnXkFtZTYwMTA0NDU5._V1_SX300.jpg"},{"Title":"Billy Lynn's Long Halftime Walk","Year":"2016","imdbID":"tt2513074","Type":"movie","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTY1NDI1Nzg4OF5BMl5BanBnXkFtZTgwNjYxOTg4ODE@._V1_SX300.jpg"},{"Title":"Long Way Round","Year":"2004","imdbID":"tt0403778","Type":"series","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTAzODU4MTgxNDleQTJeQWpwZ15BbWU3MDQ1NTgyMDI@._V1._CR15,2,339,480_SY132_CR2,0,89,132_AL_.jpg_V1_SX300.jpg"}],"totalResults":"1705","Response":"True"};
 
-click.onclick = function createList () {
-    alert("Congratulation you have clicked and you may see the cat's movie list :)")
-    document.getElementById("click").innerHTML = ("")
-    var container = document.createElement("div");
-    container.setAttribute("style", "width:700px");
-    document.body.appendChild(container);
-    for(var i = 0; i < searchResults.Search.length; i++) {
-        var arrayHolder = searchResults.Search[i];
-        var probertiesList = document.createElement("ul");
-        probertiesList.setAttribute("style","height:300px");
-        var titleItem = document.createElement("li");
-        titleItem.setAttribute("style","width:50%;float:left");
-        var titleItemText = document.createTextNode("Title:" + " " +arrayHolder.Title);
-        container.appendChild(probertiesList);
-        probertiesList.appendChild(titleItem);
-        titleItem.appendChild(titleItemText);
-        var yearItem = document.createElement("li");
-        yearItem.setAttribute("style","width:50%;float:right;clear:right");
-        var yearItemtext = document.createTextNode("Year:" + " "+ arrayHolder.Year);
-        yearItem.appendChild(yearItemtext);
-        probertiesList.appendChild(yearItem);
-        var urlItem = document.createElement("li");
-        urlItem.setAttribute("style","width:50%;float:right;clear:right");
-        var urlAncor = document.createElement("a");
-        urlAncor.setAttribute("id", "link_holder");
-        urlAncor.setAttribute("href", "http://www.imdb.com/title/" + arrayHolder.imdbID);
-        var urlText = document.createTextNode("URL: http://www.imdb.com/title/" + arrayHolder.imdbID);
-        urlAncor.appendChild(urlText);
-        /*document.getElementById("link_holder").innerHTML("http://www.imdb.com/title/" + arrayHolder.imdbID);*/ //I don't understand why this doesn't work
-        urlItem.appendChild(urlAncor);
-        probertiesList.appendChild(urlItem);
-        var imageItem = document.createElement("li");
-        imageItem.setAttribute("style","width:50%;float:right;clear:right");
-        var imageHolder = document.createElement("img");
-        imageHolder.setAttribute("src",arrayHolder.Poster);
-        imageHolder.setAttribute("style","width:50%");
-        imageItem.appendChild(imageHolder);
-        probertiesList.appendChild(imageItem);
-     }
-    
+console.log(objects.Search[2].Title);
+
+(function addElement()         {
+	var newDiv = document.createElement("div");
+	document.body.appendChild(newDiv);
+	var h1 = document.createElement('h1');
+	newDiv.appendChild(h1);
+	h1.textContent = "Movies and series";
+	newDiv.setAttribute("id", "mmyDiv");
+	var myUl = document.createElement('Ul');
+	document.getElementById('mmyDiv').appendChild(myUl);
+	myUl.setAttribute("id", "myList");
+}());
+
+
+(function ListOfMovies()       {
+    var mainList = document.getElementById('myList'), i;
+	
+    for (i = 0; i < objects.Search.length; i = i + 1) {
+		var myMovieId = objects.Search[i].Title;
+		
+		var movieEle = createListEle(myMovieId);
+		mainList.appendChild(movieEle);
+		
+		var h1 = document.createElement('h2');
+		movieEle.appendChild(h1);
+		h1.textContent =  objects.Search[i].Title;
+		
+		var p = document.createElement('p');
+		movieEle.appendChild(p);
+		p.innerHTML = "Year :  " +  objects.Search[i].Year + "<br>" + "imdbID :  " + objects.Search[i].imdbID
+			+ "<br>" + "Type :  " + objects.Search[i].Type + "<br>";
+		
+		var movUrl = document.createElement('a');
+		movieEle.appendChild(movUrl);
+		movUrl.textContent = "Click here for more";
+		movUrl.setAttribute("href", "http://www.imdb.com/title/" + objects.Search[i].imdbID);
+		movUrl.setAttribute("target", "_blank");
+		
+		var movieImg = document.createElement('img');
+		movieImg.innerHTML = objects.Search[i].Poster;
+		movieImg.setAttribute("src", objects.Search[i].Poster);
+		movieEle.appendChild(movieImg);
+    }
+	
+}());
+
+function createListEle(titl)   {
+    var listEle = document.createElement('li');
+
+    return listEle;
 }
